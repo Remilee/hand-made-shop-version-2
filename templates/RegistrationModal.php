caef511e474556a4509e432b7376d1ff9a109113
Катерина <remilee@k53sv>
2016-09-01 16:53:24 +0300

    added modal window of login

---
+++ templates/RegistrationModal.php
@@ -0,0 +1,27 @@
+  <div id="RegistrationModal" class="modal fade">
+    <div class="modal-dialog">
+    <div class="modal-content">
+    <div class="modal-header"><button class="close" type="button" data-dismiss="modal">×</button>
+    <h4 class="modal-title">Авторизация</h4>
+    </div>
+    <div class="modal-body">
+        <div class="form-group row">
+            <label for="example-text-input" class="col-xs-2 col-form-label">Логин</label>
+            <div class="col-xs-10">
+            <input class="form-control" type="text"id="example-text-input">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="example-password-input" class="col-xs-2 col-form-label">Пароль</label>
+            <div class="col-xs-10">
+            <input class="form-control" type="password" id="example-password-input">
+            </div>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-primary pull-right" data-toggle="modal" data-target = "#RegistrationModal">Регистрация</button>
+        <button class="btn btn-default" type="button" data-dismiss="modal">Закрыть</button>
+    </div>
+    </div>
+    </div>
+  </div>
--- templates/header.php
+++ templates/header.php
@@ -5,12 +5,13 @@
       <a class="nav-link" href="#">Домой <span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Войти</a>
+      <a class="nav-link" href="#" data-toggle="modal" data-target = "#AuthModal">Войти</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Блог</a>
     </li>
   </ul>
+<?php include("AuthModal.php");?>
   <form class="form-inline pull-xs-right hidden-xs-down">
     <input class="form-control" type="text" placeholder="Поиск...">
     <button class="btn btn-secondary" type="submit">Найти</button>
